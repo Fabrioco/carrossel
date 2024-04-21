1430109a07f5475b681030147f8c11f18508a19a
Fabrioco <fabriciooliveiralopes50@gmail.com>
2024-04-21 09:52:41 -0300

    feitos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import "./App.css";
+
+import img1 from "./imgs/img1.jpg";
+import img2 from "./imgs/img2.jpg";
+import img3 from "./imgs/img3.jpg";
+import img4 from "./imgs/img4.jpg";
+
+const arrfotos = [img1, img2, img3, img4];
+
+export default function App() {
+  const [contador, setContador] = useState(0);
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    if (contador === 0) {
+      document.getElementById("prev").disabled = true;
+    } else if (contador === arrfotos.length - 1) {
+      document.getElementById("next").disabled = true;
+    } else {
+      document.getElementById("prev").disabled = false;
+      document.getElementById("next").disabled = false;
+    }
+  }, [contador]);
+
+  const handleClickAdd = () => {
+    setContador(contador + 1);
+    setProgress(progress + 1);
+  };
+
+  const handleClickRemove = () => {
+    setContador(contador - 1);
+    setProgress(progress - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <img className="picExample" src={arrfotos[contador]} alt="titulo" />
+      </div>
+      <div className="carrossel_container">
+        {arrfotos.map((p, index) => (
+          <div id="img" key={index}>
+            <img className="carrossel" src={p} alt="min" />
+          </div>
+        ))}
+      </div>
+      <div>
+        <button onClick={handleClickRemove} id="prev">
+          Prev
+        </button>
+        <button onClick={handleClickAdd} id="next">
+          Next
+        </button>
+      </div>
+      {progress}
     </div>
   );
 }
-
-export default App;
